74bfaafc6860ff356f387e45b7f46da4911dd5c2
taozen <buptzhengtao@gmail.com>
2017-06-08 21:09:03 +0800

    Add DP solution for regexpMatch

--- 10-regularExpressionMatching.go
+++ 10-regularExpressionMatching.go
@@ -1,11 +1,9 @@
 package leetcode
 
-import "fmt"
-
 func matchStar(c byte, s, p string) bool {
 	//fmt.Printf("matchStar: c=%c, s=%s, p=%s\n", c, s, p)
 	for i := 0; i <= len(s); i++ {
-		if isMatch(s[i:], p) {
+		if matchHere(s[i:], p) {
 			return true
 		}
 
@@ -17,8 +15,8 @@ func matchStar(c byte, s, p string) bool {
 	return false
 }
 
-func isMatch(s string, p string) bool {
-	//fmt.Printf("isMatch: s=%s, p=%s\n", s, p)
+func matchHere(s string, p string) bool {
+	//fmt.Printf("matchHere: s=%s, p=%s\n", s, p)
 	if len(p) == 0 {
 		return len(s) == 0
 	}
@@ -28,12 +26,44 @@ func isMatch(s string, p string) bool {
 	}
 
 	if len(s) == 0 {
-		return len(p) == 0
+		return false
 	}
 
 	if p[0] != '.' && p[0] != s[0] {
 		return false
 	}
 
-	return isMatch(s[1:], p[1:])
+	return matchHere(s[1:], p[1:])
+}
+
+// Dynamic Programing, O(m*n)
+func isMatchDP(s string, p string) bool {
+	m, n := len(s), len(p)
+	dp := make([][]bool, m+1)
+
+	for i := range dp {
+		dp[i] = make([]bool, n+1)
+	}
+
+	dp[0][0] = true
+
+	for j := 1; j <= n; j++ {
+		dp[0][j] = j > 1 && p[j-1] == '*' && dp[0][j-2]
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if p[j-1] != '*' {
+				dp[i][j] = (s[i-1] == p[j-1] || p[j-1] == '.') && dp[i-1][j-1]
+			} else {
+				dp[i][j] = dp[i][j-2] || ((p[j-2] == s[i-1] || p[j-2] == '.') && dp[i-1][j])
+			}
+		}
+	}
+
+	return dp[m][n]
+}
+
+func isMatch(s string, p string) bool {
+	return isMatchDP(s, p)
 }
