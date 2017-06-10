d1ea48c60af9d18dda18d5e2e1ca62728108dfab
taozen <buptzhengtao@gmail.com>
2017-06-10 15:25:17 +0800

    Fix typo in intToRoman

--- 12-intToRoman.go
+++ 12-intToRoman.go
@@ -6,7 +6,7 @@ Input is guaranteed to be within the range from 1 to 3999.
 */
 
 func intToRoman(num int) string {
-	thousands := []string{"", "M", "MM", "MMM", "MMM"}
+	thousands := []string{"", "M", "MM", "MMM"}
 	hundreds := []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
 	tens := []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
 	ones := []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
