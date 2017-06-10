0e47306d02590fc37a9da03ee3edc7017424291c
taozen <buptzhengtao@gmail.com>
2017-06-10 15:10:54 +0800

    Make the vertical scanning version of LCP working

--- 14-longestCommonPrefix.go
+++ 14-longestCommonPrefix.go
@@ -4,15 +4,24 @@ package leetcode
 Write a function to find the longest common prefix string amongst an array of strings.
 */
 
-func longestCommonPrefix(ss []string) string {
+// Horizontal scaning.
+func longestCommonPrefixHS(ss []string) string {
 	if len(ss) == 0 {
 		return ""
+	}
+
+	// This optimization looks over-engineering.
+	minLen := len(ss[0])
+	for _, s := range ss[1:] {
+		if len(s) < minLen {
+			minLen = len(s)
+		}
 	}
 
 	ans := ss[0]
 	for _, s := range ss[1:] {
 		i := 0
-		for i < len(s) && i < len(ans) {
+		for i < minLen && i < len(ans) {
 			if s[i] != ans[i] {
 				break
 			}
@@ -24,8 +33,8 @@ func longestCommonPrefix(ss []string) string {
 	return ans
 }
 
-// A loop variant. It's faster in some cases, but TLE in leetcode OJ.
-func longestCommonPrefix2(ss []string) string {
+// Vertical scaning. It's faster in some cases, e.g. small len(ss) but large len(s).
+func longestCommonPrefixVS(ss []string) string {
 	if len(ss) == 0 {
 		return ""
 	}
@@ -35,11 +44,16 @@ func longestCommonPrefix2(ss []string) string {
 outer:
 	for i < slen {
 		for _, s := range ss[1:] {
-			if s[i] != ans[i] {
+			if i == len(s) || s[i] != ans[i] {
 				break outer
 			}
 		}
+		i++
 	}
 
 	return ans[:i]
+}
+
+func longestCommonPrefix(ss []string) string {
+	return longestCommonPrefixVS(ss)
 }
